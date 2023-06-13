6447843ef628847b1f9750f0a8757dfa938dcfb6
DuarteAugustoRodriguesLucas <dua.lucas@sapo.pt>
2023-06-13 17:44:05 +0100

    EmergenciaFinal

--- BraGuiaa/Ecras/Emergencia.js
+++ BraGuiaa/Ecras/Emergencia.js
@@ -1,22 +1,11 @@
 import React from 'react';
-import { View, Button, Linking, Alert, Platform } from 'react-native';
+import { View, Button } from 'react-native';
+import { Linking } from 'react-native';
 
 const Emergencia = () => {
-  const handleEmergencyCall = async () => {
-    const url = 'tel:112';
-    const canOpenURL = await Linking.canOpenURL(url);
-
-    if (__DEV__ && Platform.OS === 'ios') {
-      Alert.alert('Simulação de chamada de emergência para 112 no ambiente de desenvolvimento do iOS');
-      return;
-    }
-
-    if (canOpenURL) {
-      Linking.openURL(url)
-        .catch((err) => console.error('Failed to dial 112', err));
-    } else {
-      Alert.alert('Este dispositivo não é capaz de fazer chamadas telefônicas');
-    }
+  const handleEmergencyCall = () => {
+    Linking.openURL('tel:112')
+      .catch((err) => console.error('Failed to dial 112', err));
   };
 
   return (
